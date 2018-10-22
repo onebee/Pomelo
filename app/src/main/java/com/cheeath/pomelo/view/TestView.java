d74b269f0e4d24f8d44fc09d147f1a21c8812997
Kaibin <diaokaibin@gmail.com>
2018-10-22 16:15:47 +0800

    add test view

---
+++ app/src/main/java/com/cheeath/pomelo/view/TestView.java
@@ -0,0 +1,32 @@
+package com.cheeath.pomelo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.cheeath.pomelo.Utils;
+
+public class TestView extends View {
+
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(getWidth()/2,getHeight()/2, Utils.dp2px(150),paint);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+    }
+}
