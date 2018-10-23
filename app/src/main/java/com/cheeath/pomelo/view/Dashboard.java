530c18234cd9d5e21462fa6ae8301eabdb805577
Kaibin <diaokaibin@gmail.com>
2018-10-23 09:54:35 +0800

    update

---
+++ app/src/main/java/com/cheeath/pomelo/view/Dashboard.java
@@ -0,0 +1,36 @@
+package com.cheeath.pomelo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Dashboard extends View {
+
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path path = new Path();
+
+    {
+        paint.setStyle(Paint.Style.FILL);
+
+    }
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        path.reset();
+    }
+}
