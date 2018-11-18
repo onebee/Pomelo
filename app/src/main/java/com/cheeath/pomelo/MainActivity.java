2af95ea54b20b5d1b1f7c5f253c216508d47dc32
diaokaibin <kaibing.diao@hand-china.com>
2018-11-18 23:52:08 +0800

    add PieChart

--- app/src/main/java/com/cheeath/pomelo/MainActivity.java
+++ app/src/main/java/com/cheeath/pomelo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.cheeath.pomelo;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cheeath.pomelo.view.ClockView;
@@ -17,18 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final ClockView view = findViewById(R.id.view);
 
-        CountDownTimer countDownTimer = new CountDownTimer(30000000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                angle = angle + 6;
-                view.setSecondHand(angle);
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-        };
-        countDownTimer.start();
+//        CountDownTimer countDownTimer = new CountDownTimer(30000000, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                angle = angle + 6;
+//                view.setSecondHand(angle);
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//
+//            }
+//        };
+//        countDownTimer.start();
     }
 }
--- app/src/main/java/com/cheeath/pomelo/view/PieChart.java
+++ app/src/main/java/com/cheeath/pomelo/view/PieChart.java
@@ -2,20 +2,31 @@ package com.cheeath.pomelo.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.cheeath.pomelo.Utils;
+
 /**
  * @author diaokaibin@gmail.com on 2018/11/6.
  */
 public class PieChart extends View {
 
+
+    float RADIU = Utils.dp2px(120);
+    int LENGTH = (int) Utils.dp2px(20);
     Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     RectF bounds = new RectF();
+
+    int[] colors = {Color.parseColor("#2979FF"), Color.parseColor("#C2185B"),
+            Color.parseColor("#009688"), Color.parseColor("#FF8F00")};
+
+    int[] angle = {60, 100, 120, 80};
 
 
     {
@@ -28,8 +39,28 @@ public class PieChart extends View {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bounds.set(getWidth() / 2 - RADIU, getHeight() / 2 - RADIU, getWidth() / 2 + RADIU, getHeight() / 2 + RADIU);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        int currentAngle = 0;
+        for (int i = 0; i < angle.length; i++) {
+            mPaint.setColor(colors[i]);
+            canvas.save();
+            if (i == 2) {
+                canvas.translate((float) Math.cos(Math.toRadians(currentAngle + angle[i] / 2)) * LENGTH,
+
+                        (float) Math.sin(Math.toRadians(currentAngle + angle[i] / 2)) * LENGTH);
+            }
+
+            canvas.drawArc(bounds, currentAngle, angle[i], true, mPaint);
+            canvas.restore();
+            currentAngle += angle[i];
+        }
 
     }
 }
