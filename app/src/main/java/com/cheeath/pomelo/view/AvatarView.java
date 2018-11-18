2fd24c08c398676a79df404ccafed39993e17398
diaokaibin <kaibing.diao@hand-china.com>
2018-11-18 23:53:53 +0800

    add AvatarView

---
+++ app/src/main/java/com/cheeath/pomelo/view/AvatarView.java
@@ -0,0 +1,12 @@
+package com.cheeath.pomelo.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AvatarView extends View {
+    public AvatarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
