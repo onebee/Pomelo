f3af96103e29b6803f412820763ae395473ee67c
diaokaibin <kaibing.diao@hand-china.com>
2019-01-27 22:21:21 +0800

    before update

--- 08_animation/src/main/java/one/one/com/a08_animation/MainActivity.java
+++ 08_animation/src/main/java/one/one/com/a08_animation/MainActivity.java
@@ -51,36 +51,59 @@ public class MainActivity extends AppCompatActivity {
 //        auto_tv.startScroll();
 
 
+        long time = 500;
+        float size = 5;
+
+
         tv1 = findViewById(R.id.tv1);
         tv2 = findViewById(R.id.tv2);
         tv3 = findViewById(R.id.tv3);
         tv4 = findViewById(R.id.tv4);
+        float height = Utils.dp2px(50);
+
+        // 初始化
+        ObjectAnimator animator0 = ObjectAnimator.ofFloat(tv2, "translationY", height);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(tv3, "translationY", height);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(tv4, "translationY", height);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(10);
+        animatorSet.playTogether(animator0, animator1, animator2);
+        animatorSet.start();
+
 
         // 第一阶段
-        ObjectAnimator objectAnimatorTv1Y = ObjectAnimator.ofFloat(tv1, "translationY", -Utils.dp2px(50)).setDuration(2000);
-        ObjectAnimator objectAnimatorTv1Size = ObjectAnimator.ofFloat(tv1, "haSize", 5).setDuration(2000);
-        ObjectAnimator objectAnimatorTv1Color = ObjectAnimator.ofInt(tv1, "haColor", 5).setDuration(2000);
+        ObjectAnimator objectAnimatorTv1Y = ObjectAnimator.ofFloat(tv1, "translationY", -height);
+        ObjectAnimator objectAnimatorTv1Size = ObjectAnimator.ofFloat(tv1, "haSize", size);
+        ObjectAnimator objectAnimatorTv1Color = ObjectAnimator.ofInt(tv1, "haColor", 5);
 
-        ObjectAnimator objectAnimatorTv2Y = ObjectAnimator.ofFloat(tv2, "translationY", -Utils.dp2px(50)).setDuration(2000);
+        ObjectAnimator objectAnimatorTv2Y = ObjectAnimator.ofFloat(tv2, "translationY", height, 0);
 
         AnimatorSet animatorSet1 = new AnimatorSet();
-        animatorSet1.playTogether(objectAnimatorTv1Y, objectAnimatorTv1Size, objectAnimatorTv1Color,objectAnimatorTv2Y);
+        animatorSet1.setDuration(time);
+        animatorSet1.playTogether(objectAnimatorTv1Y, objectAnimatorTv1Size, objectAnimatorTv1Color, objectAnimatorTv2Y);
         animatorSet1.start();
 
 
         // 第二阶段
-        ObjectAnimator objectAnimatorTv1Y2 = ObjectAnimator.ofFloat(tv1, "translationY", -Utils.dp2px(50),-Utils.dp2px(100)).setDuration(2000);
-        ObjectAnimator objectAnimatorTv1Size2 = ObjectAnimator.ofFloat(tv1, "haSize", 5,0).setDuration(2000);
-        ObjectAnimator objectAnimatorTv1Color2= ObjectAnimator.ofInt(tv1, "haColor", 5,0).setDuration(2000);
+        ObjectAnimator objectAnimatorTv1Y2 = ObjectAnimator.ofFloat(tv1, "translationY", -height, -2 * height);
+        ObjectAnimator objectAnimatorTv1Size2 = ObjectAnimator.ofFloat(tv1, "haSize", size, 0);
+        ObjectAnimator objectAnimatorTv1Color2 = ObjectAnimator.ofInt(tv1, "haColor", 5, 0);
 
-        ObjectAnimator objectAnimatorTv2Y2 = ObjectAnimator.ofFloat(tv2, "translationY", -Utils.dp2px(50),-Utils.dp2px(100)).setDuration(2000);
-        ObjectAnimator objectAnimatorTv2Size2 = ObjectAnimator.ofFloat(tv2, "haSize", 5).setDuration(2000);
-        ObjectAnimator objectAnimatorTv2Color2 = ObjectAnimator.ofInt(tv2, "haColor", 5).setDuration(2000);
+        ObjectAnimator objectAnimatorTv2Y2 = ObjectAnimator.ofFloat(tv2, "translationY", 0, -height);
+        ObjectAnimator objectAnimatorTv2Size2 = ObjectAnimator.ofFloat(tv2, "haSize", size);
+        ObjectAnimator objectAnimatorTv2Color2 = ObjectAnimator.ofInt(tv2, "haColor", 5);
+
+        ObjectAnimator objectAnimatorTv3Y2 = ObjectAnimator.ofFloat(tv3, "translationY", 0);
+
 
         final AnimatorSet animatorSet2 = new AnimatorSet();
+        animatorSet2.setDuration(time);
 
 
-        animatorSet2.playTogether(objectAnimatorTv1Y2, objectAnimatorTv1Size2, objectAnimatorTv1Color2,objectAnimatorTv2Y2,objectAnimatorTv2Size2,objectAnimatorTv2Color2);
+        animatorSet2.playTogether(objectAnimatorTv1Y2, objectAnimatorTv1Size2, objectAnimatorTv1Color2,
+                objectAnimatorTv2Y2, objectAnimatorTv2Size2, objectAnimatorTv2Color2,
+                objectAnimatorTv3Y2
+        );
 
         animatorSet1.addListener(new Animator.AnimatorListener() {
             @Override
@@ -105,9 +128,162 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        // 第三阶段
+        final AnimatorSet animatorSet3 = new AnimatorSet();
+        animatorSet3.setDuration(time);
+
+        ObjectAnimator objectAnimatorTv1Y3 = ObjectAnimator.ofFloat(tv1, "translationY", -2 * height, -3 * height);
+
+        ObjectAnimator objectAnimatorTv2Y3 = ObjectAnimator.ofFloat(tv2, "translationY", -height, -2 * height);
+        ObjectAnimator objectAnimatorTv2Size3 = ObjectAnimator.ofFloat(tv2, "haSize", size, 0);
+        ObjectAnimator objectAnimatorTv2Color3 = ObjectAnimator.ofInt(tv2, "haColor", 5, 0);
+
+        ObjectAnimator objectAnimatorTv3Y3 = ObjectAnimator.ofFloat(tv3, "translationY", 0, -height);
+        ObjectAnimator objectAnimatorTv3Size3 = ObjectAnimator.ofFloat(tv3, "haSize", size);
+        ObjectAnimator objectAnimatorTv3Color3 = ObjectAnimator.ofInt(tv3, "haColor", 5);
+
+        ObjectAnimator objectAnimatorTv4Y3 = ObjectAnimator.ofFloat(tv4, "translationY", 0);
+
+        animatorSet3.playTogether(objectAnimatorTv2Y3, objectAnimatorTv2Size3, objectAnimatorTv2Color3, objectAnimatorTv1Y3,
+                objectAnimatorTv3Y3, objectAnimatorTv3Size3, objectAnimatorTv3Color3, objectAnimatorTv4Y3
+
+        );
+
+        animatorSet2.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animatorSet3.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        // 第四阶段
+
+        final AnimatorSet animatorSet4 = new AnimatorSet();
+
+        animatorSet4.setDuration(time);
 
 
+        ObjectAnimator objectAnimatorTv1Y4 = ObjectAnimator.ofFloat(tv1, "translationY", -3 * height, -4 * height);
 
+        ObjectAnimator objectAnimatorTv2Y4 = ObjectAnimator.ofFloat(tv2, "translationY", -2*height,-3 * height);
+
+        ObjectAnimator objectAnimatorTv3Y4 = ObjectAnimator.ofFloat(tv3, "translationY", -height,-2 * height);
+        ObjectAnimator objectAnimatorTv3Size4 = ObjectAnimator.ofFloat(tv3, "haSize",  0);
+        ObjectAnimator objectAnimatorTv3Color4 = ObjectAnimator.ofInt(tv3, "haColor",  0);
+
+
+        ObjectAnimator objectAnimatorTv4Y4 = ObjectAnimator.ofFloat(tv4, "translationY", 0,-height);
+        ObjectAnimator objectAnimatorTv4Size4 = ObjectAnimator.ofFloat(tv4, "haSize", size);
+        ObjectAnimator objectAnimatorTv4Color4 = ObjectAnimator.ofInt(tv4, "haColor", 5);
+
+        animatorSet4.playTogether(objectAnimatorTv1Y4,objectAnimatorTv2Y4,objectAnimatorTv3Y4,objectAnimatorTv3Size4,objectAnimatorTv3Color4,
+                objectAnimatorTv4Y4,objectAnimatorTv4Size4,objectAnimatorTv4Color4
+
+        );
+
+        animatorSet3.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animatorSet4.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        // 阶段5
+        final AnimatorSet animatorSet5 = new AnimatorSet();
+        animatorSet5.setDuration(time);
+        ObjectAnimator objectAnimatorTv1Y5 = ObjectAnimator.ofFloat(tv1, "translationY", -4 * height, -5 * height);
+        ObjectAnimator objectAnimatorTv2Y5 = ObjectAnimator.ofFloat(tv2, "translationY", -3*height,-4 * height);
+        ObjectAnimator objectAnimatorTv3Y5 = ObjectAnimator.ofFloat(tv3, "translationY", -2*height,-3 * height);
+        ObjectAnimator objectAnimatorTv4Y5 = ObjectAnimator.ofFloat(tv4, "translationY", -height,-2*height);
+        ObjectAnimator objectAnimatorTv4Size5 = ObjectAnimator.ofFloat(tv4, "haSize", 0);
+        ObjectAnimator objectAnimatorTv4Color5 = ObjectAnimator.ofInt(tv4, "haColor", 0);
+        animatorSet5.playTogether(objectAnimatorTv1Y5,objectAnimatorTv2Y5,objectAnimatorTv3Y5,objectAnimatorTv4Y5,objectAnimatorTv4Size5,objectAnimatorTv4Color5);
+
+        animatorSet4.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                animatorSet5.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        // 阶段6
+        final AnimatorSet animatorSet6 = new AnimatorSet();
+        animatorSet6.setDuration(time);
+        ObjectAnimator objectAnimatorTv1Y6 = ObjectAnimator.ofFloat(tv1, "translationY",  -5 * height,-8 *height);
+        ObjectAnimator objectAnimatorTv2Y6 = ObjectAnimator.ofFloat(tv2, "translationY", -4 * height,-7*height);
+        ObjectAnimator objectAnimatorTv3Y6 = ObjectAnimator.ofFloat(tv3, "translationY", -3 * height,-6 *height);
+        ObjectAnimator objectAnimatorTv4Y6 = ObjectAnimator.ofFloat(tv4, "translationY", -2*height,-5*height);
+
+        animatorSet6.playTogether(objectAnimatorTv1Y6,objectAnimatorTv2Y6,objectAnimatorTv3Y6,objectAnimatorTv4Y6);
+
+        animatorSet5.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                animatorSet6.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
 
 
 
