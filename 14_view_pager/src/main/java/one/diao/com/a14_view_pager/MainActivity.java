53920ba87cccc98726468669fe151187419978b0
Kaibin <diaokaibin@gmail.com>
2019-02-08 22:52:10 +0800

    add 14 viewpager

---
+++ 14_view_pager/src/main/java/one/diao/com/a14_view_pager/MainActivity.java
@@ -0,0 +1,13 @@
+package one.diao.com.a14_view_pager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
