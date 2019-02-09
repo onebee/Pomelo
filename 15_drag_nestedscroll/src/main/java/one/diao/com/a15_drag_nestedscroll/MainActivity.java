5b2188e80535e1918c9c0ce7d23959693215df94
Kaibin <diaokaibin@gmail.com>
2019-02-09 15:29:47 +0800

    add 15 drag and nested scroll

---
+++ 15_drag_nestedscroll/src/main/java/one/diao/com/a15_drag_nestedscroll/MainActivity.java
@@ -0,0 +1,13 @@
+package one.diao.com.a15_drag_nestedscroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
