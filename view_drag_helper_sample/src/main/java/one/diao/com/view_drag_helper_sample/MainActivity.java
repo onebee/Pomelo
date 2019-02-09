b90b52269c04dcdfc7e20bb9bf43d094934845e1
Kaibin <diaokaibin@gmail.com>
2019-02-09 22:19:48 +0800

    add ViewDragHelper Sample

---
+++ view_drag_helper_sample/src/main/java/one/diao/com/view_drag_helper_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package one.diao.com.view_drag_helper_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drag_viewgroup);
+    }
+}
