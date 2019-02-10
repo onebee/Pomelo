fc6fc992b11d22f6c6e02592ecfa177fe7a81092
Kaibin <diaokaibin@gmail.com>
2019-02-10 22:15:57 +0800

    add sample

---
+++ view_drag_helper_sample/src/main/java/one/diao/com/view_drag_helper_sample/CardDataItem.java
@@ -0,0 +1,12 @@
+package one.diao.com.view_drag_helper_sample;
+
+/**
+ * 卡片数据装载对象
+ *
+ */
+public class CardDataItem {
+    String imagePath;
+    String userName;
+    int likeNum;
+    int imageNum;
+}
