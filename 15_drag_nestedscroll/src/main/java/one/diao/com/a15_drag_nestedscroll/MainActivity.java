149ebe26efc92965d183aaad17d96d3cb7428773
Kaibin <diaokaibin@gmail.com>
2019-02-16 20:19:27 +0800

    add sample

--- 15_drag_nestedscroll/src/main/java/one/diao/com/a15_drag_nestedscroll/MainActivity.java
+++ 15_drag_nestedscroll/src/main/java/one/diao/com/a15_drag_nestedscroll/MainActivity.java
@@ -1,5 +1,6 @@
 package one.diao.com.a15_drag_nestedscroll;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drag_listener_grid_view);
+
+        SystemClock.sleep(2000);
     }
 }
--- 17_thread_interaction/src/main/java/one/diao/com/a17_thread_interaction/ThreadInteraction.java
+++ 17_thread_interaction/src/main/java/one/diao/com/a17_thread_interaction/ThreadInteraction.java
@@ -10,14 +10,41 @@ public class ThreadInteraction implements TestDemo {
         Thread thread = new Thread() {
             @Override
             public void run() {
+                // run 方法结束 就代表线程结束
                 super.run();
                 for (int i = 0; i < 1_000_000_000; i++) {
+//                    if (isInterrupted()) {
+//                        return;
+//                    }
+
+                    if (Thread.interrupted()) {
+                        return;
+                    }
+
+                    try {
+                         Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+//                        此处抛异常之后会把interrupt 的状态重置
+                        e.printStackTrace ();
+
+                        // 收尾操作
+//                        return;
+                    }
                     System.out.println("number : " + i);
                 }
             }
         };
 
         thread.start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        thread.stop();
+        thread.interrupt();
 
     }
 }
