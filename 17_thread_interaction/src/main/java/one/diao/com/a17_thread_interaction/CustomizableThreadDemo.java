beefcccd7dbcdde749aa79cce060ec9e42c2c45c
Kaibin <diaokaibin@gmail.com>
2019-02-16 22:49:41 +0800

    update sample

--- 17_thread_interaction/src/main/java/one/diao/com/a17_thread_interaction/CustomizableThreadDemo.java
+++ 17_thread_interaction/src/main/java/one/diao/com/a17_thread_interaction/CustomizableThreadDemo.java
@@ -5,9 +5,73 @@ package one.diao.com.a17_thread_interaction;
  */
 public class CustomizableThreadDemo implements TestDemo {
 
+    /**
+     * 无限循环的Thread
+     */
+    private CustomThread thread = new CustomThread();
+
 
     @Override
     public void runTest() {
+        thread.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.looper.setTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello one bit!");
+            }
+        });
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thread.looper.quit();
+    }
+
+    class CustomThread extends Thread {
+        Looper looper = new Looper();
+    }
+
+    class AnotherThread extends Thread {
+        Looper looper = new Looper();
+    }
+
+    class Looper {
+
+
+        Runnable task;
+        boolean quit;
+
+        synchronized void setTask(Runnable task) {
+            this.task = task;
+        }
+
+        synchronized void quit() {
+            quit = true;
+        }
+
+
+        public void loop() {
+//            super.run();
+            while (!quit) {
+                synchronized (this) {
+                    if (task != null) {
+                        task.run();
+                        task = null;
+                    }
+                }
+            }
+        }
+
 
     }
+
 }
