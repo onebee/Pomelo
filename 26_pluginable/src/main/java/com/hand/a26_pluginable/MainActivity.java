e01689ec3b5b4b5c953325636f92a81f88e3a3a2
Kaibin <diaokaibin@gmail.com>
2019-03-26 11:52:41 +0800

    反射

--- 26_pluginable/src/main/java/com/hand/a26_pluginable/MainActivity.java
+++ 26_pluginable/src/main/java/com/hand/a26_pluginable/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hand.a26_pluginable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Class clazz = Class.forName("com.hand.a26_pluginable.hidden.Util");
+            Constructor constructor = clazz.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+            Object instance = constructor.newInstance();
+            Method method = clazz.getDeclaredMethod("shout");
+            method.setAccessible(true);
+            method.invoke(instance);
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ 26_pluginable/src/main/java/com/hand/a26_pluginable/hidden/Util.java
@@ -0,0 +1,14 @@
+package com.hand.a26_pluginable.hidden;
+
+/**
+ * @author diaokaibin@gmail.com on 2019/3/26.
+ */
+ class Util {
+
+    Util() {
+    }
+
+    void shout() {
+        System.out.println("i'm shoting");
+    }
+}
