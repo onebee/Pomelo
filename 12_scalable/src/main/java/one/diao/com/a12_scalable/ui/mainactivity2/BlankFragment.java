ead6cd619c17d250332024edad8e7d2df4389692
Kaibin <diaokaibin@gmail.com>
2019-08-14 23:27:56 +0800

    update

---
+++ 12_scalable/src/main/java/one/diao/com/a12_scalable/ui/mainactivity2/BlankFragment.java
@@ -0,0 +1,35 @@
+package one.diao.com.a12_scalable.ui.mainactivity2;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import one.diao.com.a12_scalable.R;
+
+public class BlankFragment extends Fragment {
+
+    private BlankViewModel mViewModel;
+
+    public static BlankFragment newInstance() {
+        return new BlankFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.blank_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(BlankViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
