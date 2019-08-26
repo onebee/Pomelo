830446dd388b994cd29fbf9554d9c1be3c23ea61
Kaibin <diaokaibin@gmail.com>
2019-08-26 15:32:06 +0800

    add 手写ViewPager

---
+++ 14_view_pager/src/main/java/one/diao/com/a14_view_pager/TwoPager.java
@@ -0,0 +1,22 @@
+package one.diao.com.a14_view_pager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * @author diaokaibin@gmail.com on 2019-08-26.
+ */
+public class TwoPager extends ViewGroup {
+
+
+
+    public TwoPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
